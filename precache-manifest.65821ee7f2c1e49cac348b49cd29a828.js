25ab86c881ded5a85152443f99ff5ab75e5692de
tszymanski86 <49101021+tszymanski86@users.noreply.github.com>
2020-06-02 21:35:13 +0200

    build test

---
+++ precache-manifest.65821ee7f2c1e49cac348b49cd29a828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32fe8f7d7ae6e3c1354b17605ee25ca",
+    "url": "/W-Droge/index.html"
+  },
+  {
+    "revision": "2b11dffc941008dd0b66",
+    "url": "/W-Droge/static/css/main.de999280.chunk.css"
+  },
+  {
+    "revision": "5f088646eca57ce6a61a",
+    "url": "/W-Droge/static/js/2.a5ecab37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/W-Droge/static/js/2.a5ecab37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b11dffc941008dd0b66",
+    "url": "/W-Droge/static/js/main.4ebe69d2.chunk.js"
+  },
+  {
+    "revision": "34de188bd6d8d2672cdd",
+    "url": "/W-Droge/static/js/runtime-main.12aacbe1.js"
+  }
+]);
