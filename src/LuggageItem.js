441f5effc9fb3a7981d188221a24e04ee48a955e
tszymanski86 <49101021+tszymanski86@users.noreply.github.com>
2020-07-12 19:41:06 +0200

    Fix 'add Item' metod

--- src/LuggageItem.js
+++ src/LuggageItem.js
@@ -1,21 +1,45 @@
 import React from "react";
 import "./LuggageItem.css";
 
-function LuggageItem(props) {
-  const item = props.data;
+class LuggageItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: props.data,
+      name: props.data.name,
+      inputType: props.data.inputType,
+      value: props.data.value,
+      category: props.category,
+    };
 
-  function dzialaj(){
-    console.log('działam');
+    this.add = this.add.bind(this);
+    this.setValue = this.setValue.bind(this);
   }
 
-  return (
-    <div className="luggageItem">
-      <div className="luggageTitle">
-        {item.name}
+  setValue(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  add() {
+    console.log("dodaję:");
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="luggageItem">
+        <div className="luggageTitle">{this.state.item.name}</div>
+        {this.state.item.inputType && (
+          <input
+            type={this.state.item.inputType}
+            defaultValue={this.state.item.value}
+            onChange={this.setValue}
+          />
+        )}
+        <button onClick={this.add}>dodaj</button>
       </div>
-      {item.inputType && <input type={item.inputType} defaultValue={item.value}/>}
-    </div>
-  );
+    );
+  }
 }
 
 export default LuggageItem;
